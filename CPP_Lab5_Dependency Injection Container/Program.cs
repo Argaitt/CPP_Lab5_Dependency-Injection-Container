618da0dcf3b7c796c4e9281ec4f91b77788d4b7b
argaitt <argaitt@gmail.com>
2020-12-06 13:48:14 +0300

    add: config container class

--- CPP_Lab5_Dependency Injection Container/Program.cs
+++ CPP_Lab5_Dependency Injection Container/Program.cs
@@ -1,14 +1,5 @@
 ﻿using System;
-using Ninject.Modules;
-using Ninject.Activation;
-using Ninject.Components;
-using Ninject.Infrastructure;
-using Ninject.Injection;
-using Ninject.Parameters;
-using Ninject.Planning;
-using Ninject.Selection;
-using Ninject.Syntax;
-using Ninject;
+using System.Collections.Generic;
 
 namespace CPP_Lab5_Dependency_Injection_Container
 {
@@ -16,60 +7,21 @@ namespace CPP_Lab5_Dependency_Injection_Container
     {
         static void Main(string[] args)
         {
-			//SimpleConfigModule simpleConfigModule = new SimpleConfigModule();
-			//IKernel ninjectKernel = new StandardKernel(new SimpleConfigModule());
-			// там где нужно создать экземпляр ScheduleViewer мы вместо new, делаем так:
-			//ScheduleViewer scheduleViewer = ninjectKernel.Get<ScheduleViewer>();
+            
 		}
+    }
+    class DependenciesConfiguration
+    {
+        Dictionary<Type, List<Type>> dictionary = new Dictionary<Type, List<Type>>();
+        public void Register<Interface,Imp>()
+        {
+            dictionary.Add(Interface, value.add(Imp));
+        }
     }
 }
 
 
 
-class Schedule
-{
 
-}
-
-interface IScheduleManager
-{
-	Schedule GetSchedule();
-}
-
-class ScheduleManager : IScheduleManager
-{
-	public Schedule GetSchedule()
-	{
-		Schedule schedule = new Schedule();
-		return schedule;
-	}
-}
-
-class ScheduleViewer
-{
-	private IScheduleManager _scheduleManager;
-	public ScheduleViewer(IScheduleManager scheduleManager)
-	{
-		_scheduleManager = scheduleManager;
-	}
-	public void RenderSchedule()
-	{
-		_scheduleManager.GetSchedule();
-		// Do Something by render schedule... 
-	}
-}
-class SimpleConfigModule: NinjectModule
-{
-	public void Register<I, R>(I intce, R realization) 
-	{
-	}
-	public override void Load()
-	{
-		Bind<IScheduleManager>().To<ScheduleManager>();
-		// нижняя строка необязательна, это поведение стоит по умолчанию:
-		// т.е. класс подставляет сам себя
-		Bind<ScheduleViewer>().ToSelf();
-	}
-}
 
 
